0f2be89316a830161bd78e61bf08b090452dec05
DantesSin616 <asimpleperson100@gmail.com>
2025-10-29 11:25:52 -0400

    added first functions

--- src/app.js
+++ src/app.js
@@ -1,9 +1,38 @@
 #!/usr/bin/env node
 
-const args = process.argv.slice(2);
 
-if (args.includes('--help')) {
-	console.log('Usage: myApp [options]');
+/* list of task and its characteristics :
+ 
+ * tasks status [done, in progress]
+ 
+ * tasks description [normal description of the task]
+ 
+ * list all task and by category [tasks done, tasks in progress]
+ 
+ */
+
+const args = process.args.slice(2);
+
+if (args.inlcudes('--help')) {
+	console.log("Usage: TaskTracker [options]");
 	process.exit(0);
 };
 
+
+function checkForJSON() {
+
+};
+// it's going to check for a json file for the month, 
+// if there's no one it will create it.
+
+
+
+
+
+function addTask() { };
+
+
+function deleteTask() { };
+
+
+function updateTask() { };
